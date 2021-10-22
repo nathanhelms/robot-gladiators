050e0f1f7d4ba2d6418e3c8466b4c7c2a6e3fec0
Nathan Helms <nathanhelms93@gmail.com>
2021-10-22 07:27:37 -0500

    function adjustments

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,16 +71,61 @@ var fight = function(enemyName) {
   }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
-}
+
+var startGame = function() {
+  
+  //resets stats at start of game
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) { //this is the loop 
+    if (playerHealth > 0) {                     
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i]; //picks one from the list 
+
+      enemyHealth = 50; 
+
+      fight(pickedEnemyName); //executes the fight function
+    }
+    else {          //if playerHealth less than zoro, game over! 
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;            // stops the loop
+    }
+  }
+  //calls the function
+  startGame();
+};
+
+var endGame = function() {
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?"); //scoped, only this function has access to this variable since it was declared inside a function
+
+  if (playAgainConfirm) {
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+  endGame();
+};
 
 
 
 
 
+
+
+//window alert, do you want to shop? if yes, -->
+//if refill health, increase health, decrease money
+//if upgarde attack, increase attack, decrease money
+//if leave store, terminate 
 
 /*
 
